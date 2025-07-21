0b4eaf874d87e9587b8eb4db97dd6fbe19007eb0
JohnJohnsonTaren <tytar.den@gmail.com>
2025-07-21 22:31:49 +0300

    git@github.com:JohnJohnsonTaren/Module8-Dev.git

--- src/main/java/behavior/db/service/ClientDaoService.java
+++ src/main/java/behavior/db/service/ClientDaoService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface ClientDaoService {
     Client save(Client client);
+
     Client findById(long id);
+
     void update(Client client);
+
     boolean delete(long id);
+
     List <Client> findAll();
 }
